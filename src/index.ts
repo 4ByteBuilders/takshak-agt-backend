aaffdc837c79d53ca84341c37be132f2dcda43bd
Sagnik Majumder <sagnikm183@gmail.com>
2025-02-25 17:03:02 +0530

    created file structures

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import eventRouter from './routes/event.routes';
 
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
+
+app.use('/event', eventRouter);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
---
+++ src/routes/event.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+// router.post("/create",);
+// TO DO: create event router
+
+export default router;
