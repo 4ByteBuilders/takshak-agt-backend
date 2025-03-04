7876e3a20679d049975c8a1a5eefe6aa54058654
Mau_2993 <shastrimayur2993@gmail.com>
2025-03-05 00:54:26 +0530

    Admin routes created

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -4,6 +4,8 @@ import AdminController from "../controllers/admin.controller";
 
 const router = Router();
 
+router.post("/check-admin", authMiddlewareAdmin, AdminController.checkAdmin);
+router.get("/all-events", authMiddlewareAdmin, AdminController.getAllEvents);
 router.post("/create-event", authMiddlewareAdmin, AdminController.createEvent);
-
+router.delete("/delete-event", authMiddlewareAdmin, AdminController.deleteEvent);
 export default router;
