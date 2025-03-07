9c9a22783e86f6cb4d4ee69b32979aa1bfaaf854
Sagnik Majumder <sagnikm183@gmail.com>
2025-03-07 16:28:24 +0530

    modified routes

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -3,10 +3,10 @@ import { CustomError } from "../utils/CustomError";
 import logger from "../utils/logger";
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    logger.error(JSON.stringify({
+    logger.error({
         error: err.message,
         stack: err.stack,
-    }));
+    });
     const statusCode = err instanceof CustomError ? err.statusCode : 500;
 
     res.status(statusCode).json({
