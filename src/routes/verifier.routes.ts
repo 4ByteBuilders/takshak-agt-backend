1644edb63e0542cf2eef70824b76878ba0345383
Mau_2993 <shastrimayur2993@gmail.com>
2025-03-08 01:14:18 +0530

    Verify /add route changed to post

--- src/routes/verifier.routes.ts
+++ src/routes/verifier.routes.ts
@@ -5,7 +5,7 @@ import VerifierController from "../controllers/verifier.controller";
 const router = Router();
 
 router.get("/", authMiddlewareAdmin, VerifierController.getAll);
-router.get("/add", authMiddlewareAdmin, VerifierController.addVerifier);
+router.post("/add", authMiddlewareAdmin, VerifierController.addVerifier);
 router.get("/remove", authMiddlewareAdmin, VerifierController.removeVerifier);
 router.post("/booking", authMiddlewareVerifiers, VerifierController.verifyBooking);
 router.post("/check-in", authMiddlewareVerifiers, VerifierController.checkIn);
